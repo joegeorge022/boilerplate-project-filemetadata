f00d2805b9edf4d92f461c075a6184a8d7f28658
Joe <127773439+joegeorge022@users.noreply.github.com>
2025-03-23 15:27:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
 require('dotenv').config()
 
 var app = express();
@@ -11,8 +12,16 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const upload = multer({ dest: 'uploads/' });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const { originalname, mimetype, size } = req.file;
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
